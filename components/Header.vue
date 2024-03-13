c6cffe047953b92af768fe868c9bca264e01035a
Yuda <115441892+yydaaa@users.noreply.github.com>
2024-03-13 14:27:33 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="row p-3 bg-primary text-white">
+        <div class="col-lg-1">
+            <img src="~/assets/img/smk.png" alt="logo smk">
+        </div>
+        
+        <div class="col-lg-11">
+            <h1>PERPUS DIGITAL</h1>
+            <address>
+                SMKN 4 Tasikmalaya
+                Jl.Depok, Sukamenak, Purbartu
+            </address>
+        </div>
+    </div>
+</template>
