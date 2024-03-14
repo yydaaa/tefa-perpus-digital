42ee08ac49fd08da91d575d5a89fcabc639c93dd
Yuda <115441892+yydaaa@users.noreply.github.com>
2024-03-14 14:22:32 +0700

    index.vue

---
+++ pages/pengunjung/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+        <h2 class="text-center my-4">RIWAYAT pengunjung</h2>
+        <div class="my-3">
+          <input type="search" class="form-control form-control-lg rounded-5" placeholder="Filter...">
+        </div>
+        <div class="my-3 text-muted">menampilkan 1 dari 1</div>
+        <table class="table">
+          <thead>
+            <tr>
+              <td>#</td>
+              <td>NAMA</td>
+              <td>KEANGGOTAAN</td>
+              <td>WAKTU</td>
+              <td>KEPERLUAN</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1.</td>
+              <td>YUDA APRIANA SALAM</td>
+              <td>Siswa</td>
+              <td>26 Februari 2024, 13:00</td>
+              <td>MEMBACA</td>
+            </tr>
+          </tbody>
+        </table>
+        <nuxt-link to="../pengunjung/tambah">
+          <button type="submit" class="btn btn-secondary btn-lg rounded-5 px-5" style="margin-left: 90.5%;">KEMBALI</button>
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
