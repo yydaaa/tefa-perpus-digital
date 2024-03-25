129bec0b06bddac92714295f2f0b365efaa20fa6
Yuda <115441892+yydaaa@users.noreply.github.com>
2024-03-25 14:48:58 +0700

    Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -1,15 +1,26 @@
 <template>
-    <div class="row p-3 bg-primary text-white">
-        <div class="col-lg-1">
-            <img src="~/assets/img/smk.png" alt="logo smk">
-        </div>
-        
-        <div class="col-lg-11">
-            <h1>PERPUS DIGITAL</h1>
-            <address>
-                SMKN 4 Tasikmalaya
-                Jl.Depok, Sukamenak, Purbartu
-            </address>
-        </div>
+  <div class="row p-3 bg-primary text-white">
+    <div class="col-lg-1">
+      <img src="~/assets/img/logo.png" alt="logo SMKN 4">
     </div>
+
+    <div class="col-lg-11">
+      <h1>Perpus Digital</h1>
+      <div>SMKN 4 Tasikmalaya</div>
+      <address>
+        jl.Depok, Sukamenak, Purbaratu
+      </address>
+    </div>  
+  </div>
 </template>
+<style scoped>
+.bg-primary{
+  background-color: antiquewhite;
+  box-shadow: black;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+
+img{
+  width: 100px;
+}
+</style>
